aceea517dcf97957566d29f33637587588ce58be
niabicht <nj_abicht@outlook.com>
2025-09-18 10:25:05 +0200

    cleaning up and extending folder wrapper

--- Runtime/ClassesAndStructs/Addressables/AssetReferenceFolderSO.cs
+++ Runtime/ClassesAndStructs/Addressables/AssetReferenceFolderSO.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using DeadWrongGames.ZUtils;
 using Sirenix.OdinInspector;
 using UnityEditor.AddressableAssets.Settings;
@@ -14,70 +15,64 @@ namespace DeadWrongGames.ZCommon
     {
         [SerializeField, ReadOnly] List<string> _subKeys = new();
         
+        // Load all assets in folder
         public AsyncOperationHandle<IList<TAsset>> LoadAssetsAsync<TAsset>() where TAsset : Object
         {
             AsyncOperationHandle<IList<TAsset>> handle = Addressables.LoadAssetsAsync<TAsset>(_subKeys, _ => { }, Addressables.MergeMode.Union);
             return handle;
         }
-
-        public void ReleaseAssets(AsyncOperationHandle<IList<Object>> handle)
-        {
-            handle.Release(); // Is this correct?
-        }
-        
         
         // "Disable" the not applicable methods
         public override AsyncOperationHandle<Object> LoadAssetAsync()
         {
-            $"{name}: Cannot load single asset from a folder. Use LoadAssetsAsync instead. Returning default.".Log(level: ZMethodsDebug.LogLevel.Warning);
+            $"{name}: Cannot load single asset from a folder. Use LoadAssetsAsync instead. Returning default.".Log(level: ZMethodsDebug.LogLevel.Error);
             return default;
         }
         
         public override void ReleaseAsset(AsyncOperationHandle<Object> handle)
         {
-            "{name}: Cannot release a single asset. Use ReleaseAssets instead. Returning.".Log(level: ZMethodsDebug.LogLevel.Warning);
+            "{name}: Cannot release a single asset. Use ReleaseAssets instead. Returning.".Log(level: ZMethodsDebug.LogLevel.Error);
         }
         
 #if UNITY_EDITOR
+        #region InspectorFunctionalityOverrides
+        
+        /// <summary>
+        /// Can also be used, to reapply the sub keys in case assets have changed (sometimes a bit quirky, if in doubt, just press a few times or else, just clear the reference and start over.)
+        /// </summary>
         protected override void ApplyReference()
         {
-            if (_assetReference == null)
+            // Safety checks and get folder entry from either reference or key
+            if (_assetReference == null && _key == NOT_SET_STRING)
             {
-                "Folder reference is null. Returning.".Log(level: ZMethodsDebug.LogLevel.Warning);
+                "Folder reference and key are null. Returning.".Log(level: ZMethodsDebug.LogLevel.Warning);
                 return;
             }
 
-            AddressableAssetEntry folderEntry = GetReferenceEntry(_assetReference);
-            if (folderEntry == null)
+            AddressableAssetEntry folderEntry = (_key != NOT_SET_STRING) ? GetReferenceEntry(_key) : GetReferenceEntry(_assetReference); // Sometimes _assetReference weirdly thinks that it is not null when it should be. But does not make much of a difference.
+            if (folderEntry == null)  
             {
-                "No Addressables entry found for folder reference".Log(level: ZMethodsDebug.LogLevel.Warning);
+                "No Addressables entry found. Returning.".Log(level: ZMethodsDebug.LogLevel.Error);
                 return;
             }
 
-            // Make sure it really is a folder
             if (!folderEntry.IsFolder)
             {
-                $"{folderEntry.address} is not a folder entry".Log(level: ZMethodsDebug.LogLevel.Warning);
+                $"{folderEntry.address} is not a folder entry. Returning.".Log(level: ZMethodsDebug.LogLevel.Error);
                 return;
             }
-
+            
             // Clear and repopulate subKeys
             _subKeys.Clear();
-
-            foreach (AddressableAssetEntry child in folderEntry.SubAssets) // might be empty
-            {
+            foreach (AddressableAssetEntry child in folderEntry.SubAssets)
                 _subKeys.Add(child.address);
-            }
 
-            // foreach (AddressableAssetEntry child in folderEntry.children) // proper way to get children
-            //     _subKeys.Add(child.address);
-
-            $"{name}: Added {_subKeys.Count} sub keys from folder {folderEntry.address}".Log(level: ZMethodsDebug.LogLevel.Assertion);
-
-            // clear the ref itself
+            // Cleanup
+            _key = folderEntry.address;
             _assetReference = null;
-            _key = NOT_SET_STRING; // folder doesn't have a single key
-            _keyIsValid = _subKeys.Count > 0;
+            _isValid = _subKeys.Count > 0;
+            
+            $"{name}: Added {_subKeys.Count} sub keys from folder {folderEntry.address}".Log(level: ZMethodsDebug.LogLevel.Assertion);
         }
 
         protected override void ClearReference()
@@ -85,6 +80,24 @@ namespace DeadWrongGames.ZCommon
             _subKeys.Clear();
             base.ClearReference();
         }
+
+        protected override void Validate()
+        {
+            base.Validate();
+            AddressableAssetEntry folderEntry = GetReferenceEntry(_key);
+            if (folderEntry.SubAssets == null)
+            {
+                $"{name}: Sub assets are corrupted.".Log(level: ZMethodsDebug.LogLevel.Error);
+                _isValid = false;
+            }
+            else foreach (AddressableAssetEntry child in folderEntry.SubAssets.Where(child => child == null || GetReferenceEntry(child.address, doSubAssetsOnly: true) == null))
+            {
+                $"{name}: Sub key {child?.address} is not valid".Log(level: ZMethodsDebug.LogLevel.Error);
+                _isValid = false;
+            }
+            if (_isValid) $"{name}: All sub keys are valid".Log(level: ZMethodsDebug.LogLevel.Assertion);
+        }
+        #endregion
 #endif
     }
 }
--- Runtime/ClassesAndStructs/Addressables/BaseAssetReferenceSO.cs
+++ Runtime/ClassesAndStructs/Addressables/BaseAssetReferenceSO.cs
@@ -14,21 +14,37 @@ using UnityEditor.AddressableAssets.Settings;
 
 namespace DeadWrongGames.ZCommon
 {
-
+    /// <summary>
+    /// Base ScriptableObject for type-safe Addressables references.
+    /// Use this class to create ScriptableObject wrappers around Addressable assets
+    /// so you can safely store and load assets by key, avoiding broken references
+    /// when swapping assets in the Addressables system.
+    /// </summary>
+    /// <typeparam name="TAsset">The type of the asset (AudioClip, GameObject, etc.).</typeparam>
+    /// <typeparam name="TAssetReference">The Addressables reference type (AssetReference, AssetReferenceSprite, AssetReferenceT, etc.).</typeparam>
+    /// <example>
+    /// How to use:
+    /// 1. Create a ScriptableObject of type AssetReferenceAudioClipSO.
+    /// 2. Assign an AudioClip Addressable to _assetReference in the Inspector.
+    /// 3. Press "ApplyReference" to extract the key.
+    /// 4. At runtime, load the asset like this:
+    /// _myAudioClipSO.LoadAssetAsync().Completed += handle => _audioClip = handle.Result;
+    /// </example>
     public abstract class BaseAssetReferenceSO<TAsset, TAssetReference> : ScriptableObject where TAsset : UnityEngine.Object where TAssetReference : AssetReference
     {
         protected const string NOT_SET_STRING = "X";
         
-        [SerializeField] protected TAssetReference _assetReference; // odin throws two errors when assigning from dropdown but can be ignored
+        [SerializeField] protected TAssetReference _assetReference; // odin sometimes throws two errors when assigning from dropdown but can be ignored
         [SerializeField, ReadOnly] protected string _key = NOT_SET_STRING;
    
+        // Relay methods, to be used similarly to AssetReference methods  
         public virtual AsyncOperationHandle<TAsset> LoadAssetAsync() => Addressables.LoadAssetAsync<TAsset>(_key);
         public virtual void ReleaseAsset(AsyncOperationHandle<TAsset> handle) => Addressables.Release(handle);
 
 
 #if UNITY_EDITOR
         #region InspectorFunctionality
-        [SerializeField, ReadOnly] protected bool _keyIsValid;
+        [SerializeField, ReadOnly] protected bool _isValid;
         
         private void OnValidate()
         {
@@ -36,6 +52,11 @@ namespace DeadWrongGames.ZCommon
             if (_key != NOT_SET_STRING) _assetReference = null;
         }
 
+        /// <summary>
+        /// First, assign _assetReference in the inspector.
+        /// After calling this, the the Addressables key is extracted, stored and _assetReference is cleared.
+        /// Use this in the editor to set up your ScriptableObject.
+        /// </summary>
         [Button]
         protected virtual void ApplyReference()
         {
@@ -55,31 +76,38 @@ namespace DeadWrongGames.ZCommon
             // Get the key and clean up Inspector
             _key = GetReferenceEntry(_assetReference).address;
             _assetReference = null;
-            _keyIsValid = true;
+            _isValid = true;
             
             $"{name}: New key {_key} has successfully been assigned.".Log(level: ZMethodsDebug.LogLevel.Assertion);
         }
 
+        /// <summary>
+        /// Clears the current key and asset reference. Can be used to reset the ScriptableObject.
+        /// </summary>
         [Button]
         protected virtual void ClearReference()
         {
             _assetReference = null;
             _key = NOT_SET_STRING;
-            _keyIsValid = false;
+            _isValid = false;
         }
         
+        /// <summary>
+        /// Validates that the stored key exists in the Addressables system.
+        /// Updates the _isValid flag accordingly.
+        /// </summary>
         [Button]
-        private void Validate()
+        protected virtual void Validate()
         {
-            _keyIsValid = false;
+            _isValid = false;
             if (string.IsNullOrEmpty(_key) || _key == NOT_SET_STRING)
-                $"{name}: Key is not set".Log(level: ZMethodsDebug.LogLevel.Warning);
+                $"{name}: Key is not set".Log(level: ZMethodsDebug.LogLevel.Error);
             else if (GetReferenceEntry(_key) == null)
-                $"{name}: Key {_key} is not valid".Log(level: ZMethodsDebug.LogLevel.Warning);
+                $"{name}: Key {_key} is not valid".Log(level: ZMethodsDebug.LogLevel.Error);
             else
             {
                 $"{name}: Key {_key} is valid".Log(level: ZMethodsDebug.LogLevel.Assertion);
-                _keyIsValid = true;
+                _isValid = true;
             }
         }
         
@@ -96,16 +124,23 @@ namespace DeadWrongGames.ZCommon
             {
                 instance.Validate();
 
-                if (instance._keyIsValid) validCount++;
+                if (instance._isValid) validCount++;
                 else invalidCount++;
             }
 
             $"Validated {instances.Count} assets: {validCount} valid, {invalidCount} invalid".Log(level: ZMethodsDebug.LogLevel.Info);
         }
 
-        protected static AddressableAssetEntry GetReferenceEntry(AssetReference assetReference) => GetReferenceEntry(entry => entry.guid == assetReference.AssetGUID);
-        private static AddressableAssetEntry GetReferenceEntry(string key) => GetReferenceEntry(entry => entry.address == key);
-        private static AddressableAssetEntry GetReferenceEntry(Func<AddressableAssetEntry, bool> compareFunc)
+        /// <summary>
+        /// Finds the Addressable entry corresponding to an AssetReference or a key string.
+        /// Optional parameter <paramref name="doSubAssetsOnly"/> can be used e.g. for folder references to search only in sub-assets.
+        /// </summary>
+        /// <param name="assetReference">The AssetReference to look up.</param>
+        /// <param name="doSubAssetsOnly">Whether to search only in sub-assets.</param>
+        /// <returns>The AddressableAssetEntry if found; otherwise null.</returns>
+        protected static AddressableAssetEntry GetReferenceEntry(AssetReference assetReference, bool doSubAssetsOnly = false) => GetReferenceEntry(entry => entry.guid == assetReference.AssetGUID, doSubAssetsOnly);
+        protected static AddressableAssetEntry GetReferenceEntry(string key, bool doSubAssetsOnly = false) => GetReferenceEntry(entry => entry.address == key, doSubAssetsOnly);
+        private static AddressableAssetEntry GetReferenceEntry(Func<AddressableAssetEntry, bool> compareFunc, bool doSubAssetsOnly)
         {
             // Safety check
             AddressableAssetSettings settings = AddressableAssetSettingsDefaultObject.Settings;
@@ -116,15 +151,21 @@ namespace DeadWrongGames.ZCommon
             }
                 
             // Find the key
-            AddressableAssetEntry referenceEntry = settings.groups.Where(group => group != null).SelectMany(group => group.entries).FirstOrDefault(compareFunc);
-            if (referenceEntry == null)
+            foreach (AddressableAssetEntry entry in settings.groups.Where(group => group != null).SelectMany(group => group.entries))
             {
-                "No Addressables entry found. Returning null".Log(level: ZMethodsDebug.LogLevel.Error);
-                return null;
+                if (doSubAssetsOnly)
+                {
+                    // Loop over all children
+                    foreach (AddressableAssetEntry subEntry in entry.SubAssets.Where(compareFunc))
+                        return subEntry;
+                }
+                else if (compareFunc(entry))
+                    return entry;
             }
             
-            // Return the key
-            return referenceEntry;
+            // Otherwise log error
+            "No Addressables entry found. Returning null".Log(level: ZMethodsDebug.LogLevel.Error);
+            return null;
         }
         #endregion
 #endif
